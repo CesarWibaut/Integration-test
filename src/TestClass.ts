e1aa649dc7fc701f01b8f7fe93bb3ef14f1ee40d
Cesar Wibaut <cesarwibaut@mbp-de-cesar.lille.fr.ibm.com>
2019-09-03 16:15:23 +0200

    docs(jsdoc): Adding some jsdoc to the functions

--- src/TestClass.ts
+++ src/TestClass.ts
@@ -1,14 +1,29 @@
+/**
+ * Class used to test the package
+ */
 export default class TestClass {
+
+  /**
+   * Function that returns Hello, World!
+   */
   public helloWorld(): string {
     return 'Hello, World!';
   }
 
+  /**
+   * Async function that returns 1 after one second
+   */
   public async testingAsync(): Promise<number> {
     await this.sleep();
     return 1;
   }
 
-  private async sleep(): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, 1000));
+   /**
+    * Simple async function that just waits
+    * @param ms time in millisecond
+    */
+  private async sleep(ms: number = 1000): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
+
 }
